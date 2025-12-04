68debcc722ab48baa392c7b44a33e89a4fb0b36f
Stefan Maier <stefan.maier.98@t-online.de>
2025-12-04 19:20:18 +0100

    feat(Day2): solution for second puzzle

--- src/Day1/SecretEntrance.cs
+++ src/Day1/SecretEntrance.cs
@@ -4,24 +4,80 @@ namespace Advent2025.Day1
     {
         public class Diallock()
         {
-            private long DialNumber = 50;
-            public long amountOfZeros = 0;
+            public enum Direction
+            {
+                Left,
+                Right,
+            }
+
+            public long DialNumber = 50;
+            public long amountOfZerosStoppedAt = 0;
+            public long amountOfZerosPassed = 0;
 
             public void TurnLock(string dialInstruction)
             {
-                bool turnLeft = dialInstruction.StartsWith("L");
-                if (turnLeft)
+                Direction direction = dialInstruction[0] == 'L' ? Direction.Left : Direction.Right;
+                var dialing = long.Parse(dialInstruction[1..]);
+
+                dialing = NormalizeDialNumber(dialing);
+                if (dialing <= 0)
                 {
-                    DialNumber -= long.Parse(dialInstruction[1..].ToString());
+                    return;
+                }
+
+                MoveDial(dialing, direction);
+
+                if (IsStoppedAtZero())
+                {
+                    DialNumber = DialNumber % 100;
+                    amountOfZerosStoppedAt++;
+                }
+            }
+
+            private long NormalizeDialNumber(long dialClicks)
+            {
+                while (dialClicks > 100)
+                {
+                    dialClicks = dialClicks - 100;
+                    amountOfZerosPassed++;
+                }
+                return dialClicks;
+            }
+
+            private void MoveDial(long dialClicks, Direction direction)
+            {
+                if (direction == Direction.Right)
+                {
+                    if (DialNumber + dialClicks >= 100)
+                        amountOfZerosPassed++;
+
+                    DialNumber += dialClicks;
+                    if (DialNumber > 100)
+                    {
+                        DialNumber = DialNumber - 100;
+                    }
                 }
                 else
                 {
-                    DialNumber += long.Parse(dialInstruction[1..].ToString());
+                    if (DialNumber != 0 && DialNumber - dialClicks <= 0)
+                        amountOfZerosPassed++;
+
+                    DialNumber -= dialClicks;
+
+                    if (DialNumber <= 0)
+                    {
+                        DialNumber = 100 - Math.Abs(DialNumber);
+                    }
+                    if (DialNumber == 100)
+                    {
+                        DialNumber = 0;
+                    }
                 }
-                if (DialNumber % 100 == 0)
-                {
-                    amountOfZeros++;
-                }
+            }
+
+            private bool IsStoppedAtZero()
+            {
+                return DialNumber % 100 == 0;
             }
         }
     }
--- test/Day1/SecretEntranceTest.cs
+++ test/Day1/SecretEntranceTest.cs
@@ -18,7 +18,106 @@ namespace Advent2025.Day1.Test
             {
                 diallock.TurnLock(instruction);
             }
-            diallock.amountOfZeros.Should().Be(expectedAmountOfZerosDialed);
+            diallock.amountOfZerosStoppedAt.Should().Be(expectedAmountOfZerosDialed);
+        }
+
+        [Theory]
+        [InlineData("Day1/Example.txt", 6)]
+        [InlineData("Day1/Input.txt", 6475)]
+        public void InstructionsDialThroughTheExpectedAmountOfZeros(
+            string path,
+            long expectedAmountOfZerosDialed
+        )
+        {
+            var instructions = File.ReadAllLines(Path.GetFullPath(path));
+            SecretEntrance.Diallock diallock = new();
+            foreach (string instruction in instructions)
+            {
+                diallock.TurnLock(instruction);
+            }
+            var amountOfZerosVisited = diallock.amountOfZerosPassed;
+            amountOfZerosVisited.Should().Be(expectedAmountOfZerosDialed);
+        }
+
+        [Fact]
+        public void DialingDownFrom0OnlyIncreasesPassedCounter()
+        {
+            SecretEntrance.Diallock diallock = new();
+            diallock.DialNumber = 0;
+            diallock.TurnLock("L60");
+            diallock.amountOfZerosPassed.Should().Be(0);
+            diallock.amountOfZerosStoppedAt.Should().Be(0);
+        }
+
+        [Fact]
+        public void DialingOver100PassesZeroOnce()
+        {
+            SecretEntrance.Diallock diallock = new();
+            diallock.DialNumber = 95;
+            diallock.TurnLock("R60");
+            diallock.amountOfZerosPassed.Should().Be(1);
+            diallock.amountOfZerosStoppedAt.Should().Be(0);
+        }
+
+        [Fact]
+        public void DialingUnder0PassesZeroOnce()
+        {
+            SecretEntrance.Diallock diallock = new();
+            diallock.DialNumber = 25;
+            diallock.TurnLock("L60");
+            diallock.amountOfZerosPassed.Should().Be(1);
+            diallock.amountOfZerosStoppedAt.Should().Be(0);
+        }
+
+        [Fact]
+        public void DialingUp1000PassesZeroTenTimes()
+        {
+            SecretEntrance.Diallock diallock = new();
+            diallock.TurnLock("R1000");
+            diallock.amountOfZerosPassed.Should().Be(10);
+            diallock.amountOfZerosStoppedAt.Should().Be(0);
+        }
+
+        [Fact]
+        public void DialingDown1000PassesZeroTenTimes()
+        {
+            SecretEntrance.Diallock diallock = new();
+            diallock.DialNumber = 0;
+            diallock.TurnLock("L1000");
+            diallock.amountOfZerosPassed.Should().Be(9);
+            diallock.amountOfZerosStoppedAt.Should().Be(1);
+        }
+
+        [Fact]
+        public void DialingDown1001PassesZeroTenTimes()
+        {
+            SecretEntrance.Diallock diallock = new();
+            diallock.DialNumber = 0;
+            diallock.TurnLock("L1001");
+            diallock.amountOfZerosPassed.Should().Be(10);
+            diallock.amountOfZerosStoppedAt.Should().Be(0);
+            diallock.DialNumber = 99;
+        }
+
+        [Fact]
+        public void DialingUp1001PassesZeroTenTimes()
+        {
+            SecretEntrance.Diallock diallock = new();
+            diallock.DialNumber = 0;
+            diallock.TurnLock("R1001");
+            diallock.amountOfZerosPassed.Should().Be(10);
+            diallock.amountOfZerosStoppedAt.Should().Be(0);
+            diallock.DialNumber = 1;
+        }
+
+        [Fact]
+        public void DialingDownFrom50a1000TimesPassesZeroTenTimes()
+        {
+            SecretEntrance.Diallock diallock = new();
+            diallock.DialNumber = 50;
+            diallock.TurnLock("L1000");
+            diallock.amountOfZerosPassed.Should().Be(10);
+            diallock.amountOfZerosStoppedAt.Should().Be(0);
         }
     }
 }
