04f9076057aa02798892566b6181f955696e230a
Stefan Maier <stefan.maier.98@t-online.de>
2025-12-13 16:32:50 +0100

    refactor(Day6): simplify first solution

--- src/Day6/TrashCompactor.cs
+++ src/Day6/TrashCompactor.cs
@@ -1,4 +1,43 @@
 namespace Advent2025.Day6
 {
-    public static class TrashCompactor { }
+    public static class TrashCompactor
+    {
+        public class MathHomeWork
+        {
+            public long SolveFirstPart(List<string> input)
+            {
+                long sumOfResult = 0;
+                var puzzle = input
+                    .Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+                    .ToList();
+                for (int i = 0; i < puzzle[0].Count(); i++)
+                {
+                    var mathProblem = puzzle.Select(x => x[i]).ToList();
+                    var operation = mathProblem[^1];
+                    var terms = mathProblem[..^1];
+                    sumOfResult += SolveMathProblem(operation, terms);
+                }
+                return sumOfResult;
+            }
+
+            private static long SolveMathProblem(string operation, List<string> terms)
+            {
+                long result = 0;
+                if (operation == "+")
+                {
+                    foreach (var term in terms)
+                    {
+                        result += long.Parse(term.ToString());
+                    }
+                    return result;
+                }
+                result = 1;
+                foreach (var term in terms)
+                {
+                    result *= long.Parse(term.ToString());
+                }
+                return result;
+            }
+        }
+    }
 }
--- test/Day6/TrashCompactorTest.cs
+++ test/Day6/TrashCompactorTest.cs
@@ -10,35 +10,8 @@ namespace Advent2025.Day6.Test
         public void CheckIfNumberOfFreshFoodIsCorrect(string path, long expectation)
         {
             var inputLines = File.ReadAllLines(Path.GetFullPath(path)).ToList();
-            var puzzle = inputLines
-                .Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries))
-                .ToList();
-            long endResult = 0;
-            for (int i = 0; i < puzzle[0].Count(); i++)
-            {
-                var mathProblem = puzzle.Select(x => x[i]).ToList();
-                var operation = mathProblem[^1];
-                var terms = mathProblem[..^1];
-                if (operation == "+")
-                {
-                    long result = 0;
-                    foreach (var term in terms)
-                    {
-                        result += long.Parse(term.ToString());
-                    }
-                    endResult += result;
-                }
-                else
-                {
-                    long result = 1;
-                    foreach (var term in terms)
-                    {
-                        result *= long.Parse(term.ToString());
-                    }
-                    endResult += result;
-                }
-            }
-            endResult.Should().Be(expectation);
+            TrashCompactor.MathHomeWork homework = new();
+            homework.SolveFirstPart(inputLines).Should().Be(expectation);
         }
     }
 }
